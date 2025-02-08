2dcbb305c565c0f4cdea8c156101dd8c04a4af58
Adwaith Jayasankar <kichu31084@gmail.com>
2025-02-08 17:56:42 +0530

    Fix: Fixed the env file issue

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-  cloud_name: 'dhgfdjfb8',
-  api_key: '177322677672846',
-  api_secret: '450URwZpFdHxxLTkvBdqBbdpjiI'
+  cloud_name: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME,
+  api_key: import.meta.env.VITE_CLOUDINARY_API_KEY,
+  api_secret: import.meta.env.VITE_CLOUDINARY_API_SECRET
 });
 
 export default cloudinary; 
